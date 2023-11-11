5626b13a4ae03e1bff647aaf965165e169f178c7
MertvyDead <150365057+MertvyDead@users.noreply.github.com>
2023-11-11 21:55:25 +0300

    modified:   main.py 	new file:   task5.py 	new file:   task6.py 	new file:   task7.py 	new file:   task8v1.py 	new file:   task8v2.py

---
+++ task6.py
@@ -0,0 +1,5 @@
+a = ['z','x','c','z','x','c','z','r','z','x','c']
+s=''
+for i in a:
+    s+='\n'+i
+print(s)
---
+++ task8v2.py
@@ -0,0 +1,7 @@
+from collections import Counter
+l=[1,2,1,1,1,1,2,2,1,1,2]
+def cnt(d):
+    count=Counter(d)
+    return(count)
+print(cnt(l))
+    
