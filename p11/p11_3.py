87008d13968df2a03932a1138c6ef200836226ff
MertvyDead <150365057+MertvyDead@users.noreply.github.com>
2023-11-18 19:31:16 +0300

    modified:   p11/p11_3.py

--- p11/p11_3.py
+++ p11/p11_3.py
@@ -3,24 +3,24 @@
 
 
 def add_user(credentials_list, login, password):
-    # Проверяем, что логин не повторяется в списке словарей
+    
     for user in credentials_list:
         if user['login'] == login:
             print("Логин уже существует")
             return credentials_list
     
-    # Проверяем, что пароль длиной не менее 8 символов
+    
     if len(password) < 8:
         print("Пароль должен быть не менее 8 символов")
         return credentials_list
     
-    # Создаем новый словарь с переданным логином и паролем
+    
     new_user = {'login': login, 'password': password}
     
-    # Добавляем новый словарь в список
+    
     credentials_list.append(new_user)
     
-    # Возвращаем обновленный список
+   
     return credentials_list
 dctlst = [{'login':'mertvyDead','password':'qwerty12345'}]
 print(add_user(dctlst, input('login: '), input('password: ')))
